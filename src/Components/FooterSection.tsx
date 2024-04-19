3d4d835b712063f33fd6a056bdd06d0ee2f6d0f4
Pradeep Bisht <pb63827@gmail.com>
2024-04-19 17:26:14 +0530

    minor bug fix

--- src/Components/FooterSection.tsx
+++ src/Components/FooterSection.tsx
@@ -4,7 +4,7 @@ interface contentObj {
     url: string
 }
 
-function FooterSection({ heading, content }: { heading: string, content: [contentObj] }) {
+function FooterSection({ heading, content }: { heading: string, content: contentObj[] }) {
     return (
         <div className="section my-[3rem]">
             <h1 className='font-["Neue Montreals"] pb-[.5rem]'>{heading} :</h1>
